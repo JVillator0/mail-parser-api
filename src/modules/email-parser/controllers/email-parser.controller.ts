9a644c5c08a523cc1e9f5ca15b4ecfe8e38c3c28
Javier Villatoro <javier99.villatoro@gmail.com>
2025-03-11 14:30:04 -0600

    🐛 Fix no content response

--- src/modules/email-parser/controllers/email-parser.controller.ts
+++ src/modules/email-parser/controllers/email-parser.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ParseEmailDto } from '../dto/parse-email.dto';
 import { EmailParserService } from '../services/email-parser.service';
@@ -9,7 +9,6 @@ export class EmailParserController {
   constructor(private readonly emailParserService: EmailParserService) {}
 
   @Post('parse')
-  @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Parses an email and extracts JSON' })
   @ApiResponse({ status: 200, description: 'JSON extracted from the email' })
   @ApiResponse({ status: 204, description: 'No JSON found in the email' })
@@ -19,6 +18,10 @@ export class EmailParserController {
       parseEmailDto.emailPath,
     );
 
-    return jsonData || null;
+    if (!jsonData) {
+      throw new HttpException('', 204);
+    }
+
+    return jsonData;
   }
 }
