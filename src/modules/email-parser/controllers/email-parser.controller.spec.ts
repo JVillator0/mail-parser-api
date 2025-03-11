0cf3cb736f19242f9863ce1bafabcb089a1e56c8
Javier Villatoro <javier99.villatoro@gmail.com>
2025-03-11 12:56:13 -0600

    🧪 Adding test cases in servics and controllers

--- src/modules/email-parser/controllers/email-parser.controller.spec.ts
+++ src/modules/email-parser/controllers/email-parser.controller.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { join } from 'path';
+import { ParseEmailDto } from '../dto/parse-email.dto';
+import { EmailParserService } from '../services/email-parser.service';
 import { EmailParserController } from './email-parser.controller';
 
 describe('EmailParserController', () => {
   let controller: EmailParserController;
+  let service: EmailParserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmailParserController],
+      providers: [
+        {
+          provide: EmailParserService,
+          useValue: {
+            parseEmail: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<EmailParserController>(EmailParserController);
+    service = module.get<EmailParserService>(
+      EmailParserService,
+    ) as jest.Mocked<EmailParserService>;
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return parsed JSON from service', async () => {
+    const emailPath = join(__dirname, '../../../../test/data/json-in-body.eml');
+    const mockResponse = {
+      clients: [{ name: 'John Doe' }, { name: 'Jane Doe' }],
+    };
+
+    (service.parseEmail as jest.Mock).mockResolvedValue(mockResponse);
+
+    const dto: ParseEmailDto = { emailPath };
+    const response = await controller.parseEmail(dto);
+
+    expect(response).toEqual(mockResponse);
+    jest.spyOn(service, 'parseEmail').mockResolvedValue(mockResponse);
+  });
+
+  it('should handle errors and throw an exception', async () => {
+    const emailPath = join(__dirname, '../../../../test/data/without-json.eml');
+    (service.parseEmail as jest.Mock).mockRejectedValue(
+      new Error('No JSON found in the email.'),
+    );
+
+    const dto: ParseEmailDto = { emailPath };
+
+    await expect(controller.parseEmail(dto)).rejects.toThrow(
+      'No JSON found in the email.',
+    );
   });
 });
--- src/modules/email-parser/services/email-parser.service.spec.ts
+++ src/modules/email-parser/services/email-parser.service.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import axios from 'axios';
+import { join } from 'path';
 import { EmailParserService } from './email-parser.service';
+
+// Mock the HTTP request
+jest.mock('axios');
 
 describe('EmailParserService', () => {
   let service: EmailParserService;
@@ -12,7 +17,46 @@ describe('EmailParserService', () => {
     service = module.get<EmailParserService>(EmailParserService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should parse a JSON attachment from a local .eml file', async () => {
+    const emailPath = join(
+      __dirname,
+      '../../../../test/data/json-attached.eml',
+    );
+    const jsonData = await service.parseEmail(emailPath);
+
+    expect(jsonData).toBeDefined();
+    expect(jsonData).toHaveProperty('clients');
+
+    if (jsonData) {
+      expect(jsonData.clients).toEqual([
+        { name: 'John Doe' },
+        { name: 'Jane Doe' },
+      ]);
+    }
+  });
+
+  it('should extract JSON from a URL inside the email body', async () => {
+    const emailPath = join(__dirname, '../../../../test/data/json-in-body.eml');
+
+    (axios.get as jest.Mock).mockResolvedValueOnce({
+      data: { clients: [{ name: 'John Doe' }, { name: 'Jane Doe' }] },
+    });
+
+    const jsonData = await service.parseEmail(emailPath);
+
+    expect(jsonData).toBeDefined();
+    expect(jsonData).toHaveProperty('clients');
+
+    if (jsonData) {
+      expect(jsonData.clients).toEqual([
+        { name: 'John Doe' },
+        { name: 'Jane Doe' },
+      ]);
+    }
+  });
+
+  it('should return null if no JSON is found', async () => {
+    const emailPath = join(__dirname, '../../../../test/data/without-json.eml');
+    await expect(service.parseEmail(emailPath)).resolves.toBeNull();
   });
 });
