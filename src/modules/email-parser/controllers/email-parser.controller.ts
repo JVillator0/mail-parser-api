e103df300686e0b5195d9c923b8c73a7bc167132
Javier Villatoro <javier99.villatoro@gmail.com>
2025-03-11 14:34:04 -0600

    ✨ Adding http status 200

--- src/modules/email-parser/controllers/email-parser.controller.ts
+++ src/modules/email-parser/controllers/email-parser.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ParseEmailDto } from '../dto/parse-email.dto';
 import { EmailParserService } from '../services/email-parser.service';
@@ -9,6 +16,7 @@ export class EmailParserController {
   constructor(private readonly emailParserService: EmailParserService) {}
 
   @Post('parse')
+  @HttpCode(HttpStatus.OK)
   @ApiOperation({ summary: 'Parses an email and extracts JSON' })
   @ApiResponse({ status: 200, description: 'JSON extracted from the email' })
   @ApiResponse({ status: 204, description: 'No JSON found in the email' })
