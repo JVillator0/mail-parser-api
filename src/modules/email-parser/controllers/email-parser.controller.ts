9df1d384d92124180d0e7388c5919b40b7589600
Javier Villatoro <javier99.villatoro@gmail.com>
2025-03-11 14:12:06 -0600

    ✨ Return the correct HTTP Status if the EML file does not contain JSON files

--- src/modules/email-parser/controllers/email-parser.controller.ts
+++ src/modules/email-parser/controllers/email-parser.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ParseEmailDto } from '../dto/parse-email.dto';
 import { EmailParserService } from '../services/email-parser.service';
@@ -9,10 +9,16 @@ export class EmailParserController {
   constructor(private readonly emailParserService: EmailParserService) {}
 
   @Post('parse')
+  @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Parses an email and extracts JSON' })
   @ApiResponse({ status: 200, description: 'JSON extracted from the email' })
+  @ApiResponse({ status: 204, description: 'No JSON found in the email' })
   @ApiResponse({ status: 400, description: 'Incorrect format' })
   async parseEmail(@Body() parseEmailDto: ParseEmailDto) {
-    return this.emailParserService.parseEmail(parseEmailDto.emailPath);
+    const jsonData = await this.emailParserService.parseEmail(
+      parseEmailDto.emailPath,
+    );
+
+    return jsonData || null;
   }
 }
